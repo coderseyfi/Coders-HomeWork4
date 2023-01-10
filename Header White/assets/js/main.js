344c7921a9f9fbd6e62b78af44a0a5805072de59
coderseyfi <seyfaddinmn@code.edu.az>
2023-01-10 15:44:35 +0400

    first commit

---
+++ Header White/assets/js/main.js
@@ -0,0 +1,7 @@
+let navPhone = document.querySelector(".list-phone")
+let burgerMenu = document.querySelector(".burg-menu")
+
+burgerMenu.addEventListener('click',()=>{
+    navPhone.classList.toggle("active-bar");
+    console.log("text");
+})
